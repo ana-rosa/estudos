ec9e340076df94101be8f2968ab370e4e35547dc
Ana Rosa <anarosa.394@gmail.com>
2020-12-24 18:53:35 -0300

    Implementa operações e suporte a float.

--- calculadora.py
+++ calculadora.py
@@ -6,22 +6,20 @@ Calculadora básica
 - Subtração
 - Divisão
 - Multiplicação
-
 - Calculadora barata de 1 operação com memória -> Recebe operação como texto e retorna valor
     - Soma
-
     - Subtração
-
     - Multiplicação
     - Divisão
+- Suporte a float
+
+- Interface -> texto? CLI?
     $ python calculadora.py
     Digite a expressão:
     1 + 4
     = 5
     - 2
     = 3
-- Suporte a float
-- Interface -> texto? CLI?
 """
 
 
@@ -53,15 +51,19 @@ def multiplicacao(a, b):
 
 def processa_expressao(expressao):
     a, operacao, b = expressao.split()
-    return int(a), int(b), operacao
+    return float(a), float(b), operacao
 
 
 def calculadora(expressao):
-    # entrada_crua = "1 + 5"
-    expressao = processa_expressao(expressao)
-    a = expressao[0]
-    b = expressao[1]
-    resultado = soma(a,b)
+    a, b, operacao = processa_expressao(expressao)
+    if operacao == "-":
+        resultado = subtracao(a,b)
+    elif operacao == "+":
+        resultado = soma(a,b)
+    elif operacao == "*":
+        resultado = multiplicacao(a,b)
+    elif operacao == "/":
+        resultado = divisao(a,b)
     return resultado
 
 
@@ -93,6 +95,22 @@ def test_processa_expressao():
     assert processa_expressao("100 / 1") == (100, 1, '/')
 
 
-def test_calculadora():
+def test_calculadora_soma():
     assert calculadora("1 + 5") == 6
     assert calculadora("5 + 3") == 8
+
+def test_calculadora_subtracao():
+    assert calculadora ("3 - 2") == 1
+    assert calculadora ("1 - 4") == -3
+
+def test_calculadora_multiplicacao():
+    assert calculadora ("4 * 5") == 20
+    assert calculadora ("100 * 6") == 600
+
+def test_calculadora_divisao():
+    assert calculadora ("4 / 2") == 2
+
+def test_calculadora_float():
+    assert calculadora ("3 / 2") == 1.5
+    assert calculadora ("1.5 + 1") == 2.5 
+
